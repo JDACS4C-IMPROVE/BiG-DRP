5b6b2b9c286cc12287dd40c429352b41f52e0990
Rohan Gnanaolivu <ac.rgnanaolivu@lambda0.cels.anl.gov>
2023-02-09 06:54:16 -0800

    installed and got main runing but found bug

---
+++ conda_steps.sh
@@ -0,0 +1,19 @@
+#conda version
+conda 22.11.1
+
+
+#create conda
+conda create -n python_BigDRP python=3.8 anaconda
+
+
+#activate conda
+conda activate python_BigDRP
+
+
+
+#install pip
+conda install pip
+
+
+#install requirements
+pip install -r requirements.txt
